a70e0760f0c4bc33d19d4177140f6827029a559f
phowell212 <phowell2@stevens.edu>
2021-12-16 20:18:23 -0500

    6

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <unistd.h>
 //#include <fstream>
 #define CSIZE 52
 using namespace std;
@@ -19,23 +20,39 @@ void getcords(const char* GPGGA, int size, char* cords){
     }
 }
 
+// detection if the enter key is being pressed
+int kbhit(){
+    struct timeval tv{};
+    fd_set fds;
+    tv.tv_sec = 0;
+    tv.tv_usec = 0;
+    FD_ZERO(&fds);
+    FD_SET(STDIN_FILENO, &fds); //STDIN_FILENO is 0
+    select(STDIN_FILENO+1, &fds, nullptr, nullptr, &tv);
+    return FD_ISSET(STDIN_FILENO, &fds);
+}
+
+
 int main() {
-    while(true){
+
+    // while the enter key has not been pressed yet
+    // you might have to hold the key in order for it to be registered as pressed
+    while(!kbhit()){
 
         // use "dmesg | grep tty" to get serial info and look for USB ACM device
         // data is held in /dev/ttyASSIGNEDDEVICECODE in my case the name is ttyAMC0
         char in[CSIZE*12];
         //instream.open("/dev/ttyACM0");
 
-        //char in = FUNC TO GET THE LAST LINE OF /dev/ttyACM0
+        //char in = FUNC TO GET THE LAST LINE OF /dev/ttyAMC0
 
         // making a new char array just for the cords
         char cords[CSIZE];
         getcords(in, CSIZE*12, cords);
 
         // print cords
-        for(int i = 0; i < CSIZE; i++){
-            cout<<cords[i];
+        for(char cord : cords){
+            cout<<cord;
         }
         cout<<endl;
     }
